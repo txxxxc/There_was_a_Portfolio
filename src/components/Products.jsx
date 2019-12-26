bf8a0a42db8a8a5c44f68fb774476a2bb2d313e0
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-12-26 16:48:10 +0900

    2019/12/16

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby'
+import ReactIcon from '../svg/react'
 import Img from 'gatsby-image';
 import '../styles/products.scss';
 
 
 const Products = () => {
   const data = useStaticQuery(graphql`
-    query Products {
-      file(relativePath: { eq: "subject-simulator.png" }) {
+    query MyQuery {
+      file(
+        relativePath: { eq: "subject-simulator.png" }
+      ) {
         childImageSharp {
-          fixed(width: 500, height: 300) {
+          fluid {
             src
-            originalName
+            aspectRatio
+            sizes
+            srcSet
           }
         }
       }
@@ -23,7 +28,24 @@ const Products = () => {
       <p className="products__header">Products</p>
       <div className="products-container">
         <div className="products-container__item">
-          <Img fixed={data.file.childImageSharp.fixed} />
+          <div className="products-container__item--image">
+            <div className="tags">
+              <div className="tags__container">
+                <div className="tags__item">
+                  <ReactIcon width="24px" height="24px"/>
+                  <span className="tags__item-text">React</span>
+                </div>
+              </div> 
+            </div>
+            <Img fluid={data.file.childImageSharp.fluid} />
+          </div>
+
+          <p className="products-container__item--header">
+            時間割シミュレーター
+          </p>
+          <p className="products-container__item--text">
+            hogehogehogehogehogehgehgoehgoehgoehogheohgoehgoehgoehoghe
+          </p>
         </div>
         <div className="products-container__item"></div>
       </div>
@@ -32,14 +54,14 @@ const Products = () => {
 }
 export default Products;
 
-export const query = graphql`
-        query {
-          file(relativePath: { eq: "subject-simulator.png" }) {
-            childImageSharp {
-              fixed(width: 500, height: 300) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        }
-      `
+// export const query = graphql`
+//         query {
+//           file(relativePath: { eq: "subject-simulator.png" }) {
+//             childImageSharp {
+//               fixed(width: 500, height: 300) {
+//                 ...GatsbyImageSharpFixed
+//               }
+//             }
+//           }
+//         }
+//       `
