015c668fa261ad94ea96b3e974592c39c7a252de
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-07-26 22:36:43 +0900

    Internページで着た

--- src/components/Internship.tsx
+++ src/components/Internship.tsx
@@ -7,12 +7,17 @@ const Internship: React.FC = () => (
   <Wrapper name="internship">
     <Container>
       <Title>Internship</Title>
-      <Content>
-        <Topic>Life is Tech!</Topic>
-        <Description>Web Service Programming Course Mentor</Description>
-        <Topic>Now</Topic>
-        <Description />
-      </Content>
+      <Contents>
+        <Content>
+          <Topic>Life is Tech!</Topic>
+          <Description>
+            Web Service Programming Course Mentor(2020/07~)
+          </Description>
+        </Content>
+        <Content>
+          <Topic>Now</Topic>
+        </Content>
+      </Contents>
     </Container>
     <Arrow to="products" />
   </Wrapper>
@@ -23,6 +28,7 @@ const Wrapper = styled(Element)`
   height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
 
 const Container = styled.div`
@@ -35,42 +41,48 @@ const Title = styled.div`
   padding-bottom: 4rem;
 `;
 
-const hoge = '3.5rem';
-const piyo = '1.75rem';
-const Content = styled.div`
+const circleDiameter = 4;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
   position: relative;
-  padding-left: 3.5rem;
+  padding-left: 5rem;
+  height: 80%;
 
   &::before {
     content: '';
     /* 幅と色 */
-    background-color: #d59533;
+    background-color: #c91c70;
     width: 2px;
     /* 位置 */
     position: absolute;
-    top: ${piyo}; /* 円のwidthの半分 */
-    left: ${piyo}; /* 円のwidthの半分 */
-    height: calc(100% - ${piyo}); /* 100% - top */
+    top: ${circleDiameter / 2}rem; /* 円のwidthの半分 */
+    left: ${circleDiameter / 2}rem; /* 円のwidthの半分 */
+    height: calc(100% - ${circleDiameter / 2}rem); /* 100% - top */
     /* 連番より後ろに表示するため */
     z-index: 0;
   }
 `;
 
+const Content = styled.div`
+  &:nth-child(1) {
+    flex-grow: 1;
+  }
+`;
+
 const Topic = styled.h2`
   font-size: 4.8rem;
-  padding-bottom: 20px;
   font-weight: 400;
 
   &::before {
     content: '';
-    background: #d59533;
-    /* 文字を中央に表示する */
-    line-height: ${hoge};
-    text-align: center;
+    background: #c91c70;
+    line-height: ${circleDiameter}rem;
     /* 円で表示する */
-    width: ${hoge};
-    height: ${hoge};
-    border-radius: ${hoge};
+    width: ${circleDiameter}rem;
+    height: ${circleDiameter}rem;
+    border-radius: ${circleDiameter}rem;
     /* .stepsでmargin-left +2rem したぶん左に戻す */
     position: absolute;
     left: 0;
@@ -80,6 +92,7 @@ const Topic = styled.h2`
 const Description = styled.p`
   font-size: 3.2rem;
   padding-top: 20px;
+  padding-bottom: 20px;
   color: #d2d2d2;
 `;
 export default Internship;
